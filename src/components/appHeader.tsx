8de1169be057b7e699068bfcbfec4ecb6fda7a8b
Supertiger <saawalkhn@googlemail.com>
2026-06-16 10:31:19 +0100

    refactor

--- src/components/appHeader.tsx
+++ src/components/appHeader.tsx
@@ -1,4 +1,3 @@
-import { css } from "@linaria/core";
 import { t } from "@lingui/core/macro";
 import morphdom from "morphdom";
 
@@ -15,42 +14,7 @@ import { CdnIcon } from "./cdnIcon";
 import { Drawer } from "./drawer";
 import { Icon } from "./icon";
 
-const pill = css`
-  display: flex;
-  align-items: center;
-  font-size: 12px;
-  background-color: var(--gray-900);
-  padding: 0 6px;
-  height: 36px;
-  border-radius: var(--radius-max);
-  border: solid 1px var(--gray-700);
-  padding-right: 12px;
-  overflow: hidden;
-  min-width: 0;
-  gap: 6px;
-  white-space: nowrap;
-  width: fit-content;
-  max-width: 100%;
-  box-sizing: border-box;
-  > .channelIcon {
-    background-color: transparent;
-  }
-  > .icon {
-    border-radius: var(--radius-max);
-    background-color: var(--gray-800);
-    padding: 4px;
-    margin-right: 4px;
-    font-size: 16px;
-    &.warn {
-      background-color: var(--warn-color);
-    }
-  }
-  > .label {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-`;
+import style from "./appHeader.module.css";
 
 const Pill = () => {
   const server = serverStore.servers.get(serverStore.currentServerId!);
@@ -67,82 +31,36 @@ const Pill = () => {
   const isServerChannel = server && channel;
 
   return (
-    <div class={pill}>
+    <div class={style.pill}>
       {icon ? (
-        <Icon name={icon} class={["icon", !authenticated && "warn"]} />
+        <Icon name={icon} class={[style.icon, !authenticated && "warn"]} />
       ) : (
         <Avatar size={24} server={server} user={user} />
       )}
       {authenticated && isServerChannel ? (
-        <CdnIcon channel={channel} size={14} class="channelIcon" />
+        <CdnIcon channel={channel} size={14} class={style.channelIcon} />
       ) : null}
-      <div class="label">{label}</div>
+      <div class={style.label}>{label}</div>
     </div>
   );
 };
 
-const header = css`
-  display: flex;
-  align-items: center;
-  height: 50px;
-  padding-left: 8px;
-  padding-right: 8px;
-  flex-shrink: 0;
-  gap: 8px;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 1111;
-  > .button {
-    border-radius: var(--radius-max);
-    flex-shrink: 0;
-    > .icon {
-      font-size: 18px;
-    }
-    &.hide {
-      display: none;
-    }
-  }
-  > .details {
-    display: flex;
-    flex: 1;
-    max-width: 100%;
-    min-width: 0;
-  }
-  .backdrop {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    height: 70px;
-
-    z-index: -1;
-
-    background: linear-gradient(
-      to bottom,
-      rgba(0, 0, 0, 0.85),
-      rgba(0, 0, 0, 0)
-    );
-    pointer-events: none;
-  }
-`;
 export const createAppHeader = () => {
   const abortController = new AbortController();
   const { signal } = abortController;
   const leftDrawerButton = (
-    <Button icon="side_navigation" class="button" />
+    <Button icon="side_navigation" class={style.button} />
   ) as HTMLButtonElement;
 
   let rightDrawerButton = (
-    <Button icon="info" class="button" />
+    <Button icon="info" class={style.button} />
   ) as HTMLButtonElement;
 
   let container = (
-    <header class={header}>
-      <div class="backdrop"></div>
+    <header class={style.header}>
+      <div class={style.backdrop}></div>
       {leftDrawerButton}
-      <div class="details">
+      <div class={style.details}>
         <Pill />
       </div>
       {rightDrawerButton}
@@ -168,7 +86,7 @@ export const createAppHeader = () => {
   let pendingAnim: Animation | null = null;
 
   const updatePill = () => {
-    const pillEl = container.querySelector(`.${pill}`) as HTMLElement;
+    const pillEl = container.querySelector(`.${style.pill}`) as HTMLElement;
 
     const oldWidth = pillEl.getBoundingClientRect().width;
 
@@ -177,7 +95,7 @@ export const createAppHeader = () => {
 
     morphdom(pillEl, <Pill />);
 
-    const newLabelEl = pillEl.querySelector(".label") as HTMLElement;
+    const newLabelEl = pillEl.querySelector("." + style.label) as HTMLElement;
 
     const newWidth = pillEl.getBoundingClientRect().width;
 
@@ -208,7 +126,7 @@ export const createAppHeader = () => {
   storeEmitter.on(
     "drawer:rightDrawerAvailable",
     (available) => {
-      rightDrawerButton.classList.toggle("hide", !available);
+      rightDrawerButton.classList.toggle(style.hide!, !available);
     },
     signal,
   );
