6e10b1a9334788989e93a71ec6c16fb0e4b79351
Supertiger <saawalkhn@googlemail.com>
2026-08-20 13:09:07 +0100

    progress to settings page

---
+++ src/pages/app-page/createSettingsRoute.tsx
@@ -0,0 +1,186 @@
+import { t } from "@lingui/core/macro";
+import { Plural, Trans } from "@trans";
+
+import { Avatar } from "../../components/avatar";
+import { Banner } from "../../components/Banner";
+import { ServerClanItem } from "../../components/serverClanItem";
+import { createSettingsDrawer } from "../../components/settings/createSettingsDrawer";
+import { h } from "../../h";
+import { accountStore } from "../../store/accountStore";
+import { friendStore } from "../../store/friendStore";
+import { serverStore } from "../../store/serverStore";
+import { FriendStatus, type RawUser } from "../../Types";
+import { storeEmitter } from "../../utils/EventEmitter";
+import { getFont } from "../../utils/font";
+import { router } from "../../utils/router";
+import { getAppHeader, type RouteContext } from "./AppPage";
+
+import style from "./createSettingsRoute.module.css";
+
+type Page = { destroy: () => void };
+
+export interface Setting {
+  id: string;
+  icon: string;
+  name: () => string;
+  path: string;
+  load: () => Promise<{ default: (context: any) => Page }>;
+}
+
+export const Settings: Setting[] = [
+  {
+    id: "account",
+    icon: "account_circle",
+    name: () => t`Account`,
+    path: "/account",
+    load: () => import("./createHomePane"),
+  },
+  {
+    id: "profile",
+    icon: "person",
+    name: () => t`Profile`,
+    path: "/profile",
+    load: () => import("./createHomePane"),
+  },
+];
+
+const NameAndTag = ({ user }: { user: RawUser }) => {
+  const font = getFont(user.profile?.font);
+
+  return (
+    <div class={style.nameAndTag}>
+      <span class={[style.username, font?.class, "font"]}>{user.username}</span>
+      <span class={style.tag}>:{user.tag}</span>
+      <span class={style.badges}>
+        {user?.profile?.clan && (
+          <span class={style.clan}>
+            <ServerClanItem clan={user?.profile?.clan} />
+          </span>
+        )}
+      </span>
+    </div>
+  );
+};
+
+const Stats = () => {
+  const serverCount = serverStore.servers.size;
+  const friendCount = [...friendStore.friends.values()].map(
+    (f) => f.status === FriendStatus.FRIENDS,
+  ).length;
+
+  return (
+    <div class={style.stats}>
+      <span class={style.stat}>
+        <Plural
+          value={serverCount}
+          _0={
+            <Trans>
+              <span class={style.full}>No</span> servers
+            </Trans>
+          }
+          one={
+            <Trans>
+              <span class={style.full}>#</span> server
+            </Trans>
+          }
+          other={
+            <Trans>
+              <span class={style.full}>#</span> servers
+            </Trans>
+          }
+        />
+      </span>
+      <span class={style.stat}>
+        <Plural
+          value={friendCount}
+          _0={
+            <Trans>
+              <span class={style.full}>No</span> friends
+            </Trans>
+          }
+          one={
+            <Trans>
+              <span class={style.full}>#</span> friend
+            </Trans>
+          }
+          other={
+            <Trans>
+              <span class={style.full}>#</span> friends
+            </Trans>
+          }
+        />
+      </span>
+    </div>
+  );
+};
+
+const Header = () => {
+  const user = accountStore.currentUser;
+  if (!user) return null;
+
+  return (
+    <div class={style.header}>
+      <div class={style.banner}>
+        <Banner user={user} />
+      </div>
+      <div class={style.overlayInfo}>
+        <Avatar user={user} size={128} />
+      </div>
+
+      <div class={[style.section, style.detailsSection]}>
+        <NameAndTag user={user} />
+
+        <Stats />
+      </div>
+    </div>
+  );
+};
+
+const createSettingsRoute = ({ leftDrawer, content }: RouteContext) => {
+  const abortController = new AbortController();
+  const { signal } = abortController;
+
+  const serverChannelList = createSettingsDrawer();
+
+  const innerContent = (<div>test</div>) as HTMLDivElement;
+
+  const render = () => {
+    content.replaceChildren(
+      <div class={style.content}>
+        <Header />
+        {innerContent}
+      </div>,
+    );
+  };
+  render();
+
+  leftDrawer.replaceChildren(serverChannelList.render());
+
+  router.createMatchListener(
+    "/app/settings/*",
+    () => {
+      const matchedRoute = Settings.find((s) =>
+        router.match("/app/settings" + s.path),
+      );
+      if (!matchedRoute) return;
+      getAppHeader()?.updateHeader({
+        icon: matchedRoute.icon,
+        label: matchedRoute.name(),
+      });
+    },
+    { signal, always: true },
+  );
+
+  storeEmitter.on("ws:authStateUpdate", render, signal);
+
+  const destroy = () => {
+    abortController.abort();
+    getAppHeader()?.updateHeader({ trigger: false });
+
+    serverChannelList.destroy();
+  };
+
+  return { destroy };
+};
+
+export default createSettingsRoute;
